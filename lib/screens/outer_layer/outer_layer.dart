ccd372a265778f8c2982d29e2e6683ecc39da8d9
Utsav Shrestha <tsvshrsth@gmail.com>
2022-01-22 17:40:07 +0545

    onboarding scene done

---
+++ lib/screens/outer_layer/outer_layer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:myapp/screens/dashboard_screen/dashboard_screen.dart';
+import 'package:myapp/screens/onboarding_screen/onboarding_screen.dart';
+import 'package:onboarding/onboarding.dart';
+
+class OuterLayer extends StatefulWidget {
+  const OuterLayer({Key? key}) : super(key: key);
+
+  @override
+  _OuterLayerState createState() => _OuterLayerState();
+}
+
+class _OuterLayerState extends State<OuterLayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ValueListenableBuilder(
+            valueListenable: Hive.box<int>("outerLayer").listenable(),
+            builder: (context, Box<int> boxs, child) {
+              int value = boxs.get("state", defaultValue: 0)!;
+              return value == 0 ? OnBoardingScreen() : DashBoardScreen();
+            }));
+  }
+}
